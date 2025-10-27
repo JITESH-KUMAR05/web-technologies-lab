cffc2c4d19f3c522ea995416568f27b03e0e10e1
jitesh kumar <jitesh.kumar05official@gmail.com>
2025-10-27 17:32:41 +0530

    changes

--- EXAM/getAndPost.java
+++ EXAM/getAndPost.java
@@ -135,11 +135,7 @@ public class DemoServlet extends HttpServlet {
 // ========================================
 
 <?xml version="1.0" encoding="UTF-8"?>
-<web-app xmlns="http://xmlns.jcp.org/xml/ns/javaee"
-         xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
-         xsi:schemaLocation="http://xmlns.jcp.org/xml/ns/javaee
-         http://xmlns.jcp.org/xml/ns/javaee/web-app_3_1.xsd"
-         version="3.1">
+<web-app>
     
     <servlet>
         <servlet-name>DemoServlet</servlet-name>
